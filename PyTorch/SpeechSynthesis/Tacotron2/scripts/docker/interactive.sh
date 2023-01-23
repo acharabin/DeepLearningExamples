a6c6e6e91371b1d14ad979433f3cbe82d6777cc1
Ubuntu <ubuntu@ip-172-16-123-31.ec2.internal>
2023-01-23 19:41:09 +0000

    [Tacotron2/PyT] Added AC-Voice-Cloning-Data

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvidia-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --ipc=host -v $PWD:/workspace/tacotron2/ tacotron2 bash
+nvidia-docker run -d --name test_tacotron2 --publish 8000:8000 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --ipc=host -v $PWD:/workspace/tacotron2/ tacotron2 bash
